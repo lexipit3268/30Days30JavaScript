2b03c0115b0466ccdf0f266494ca26e8cb42a124
Nguyen Loc <locdedoda@gmail.com>
2025-07-25 22:09:28 +0700

    added getQuote()

---
+++ src/quoteGenerator/script.js
@@ -0,0 +1,12 @@
+const api_url = "https://api.quotable.io/random";
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+async function getQuote(url) {
+   const response = await fetch(url);
+   var data = await response.json();
+   quote.innerHTML = data.content;
+   author.innerHTML = data.author;
+} 
+
+getQuote(api_url);
