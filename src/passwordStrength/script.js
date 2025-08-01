7b1f1da50d3b7793d9a72ea99f8ef16b815abccb
Nguyen Loc <locdedoda@gmail.com>
2025-08-01 15:08:17 +0700

    finish passwordStrength

--- src/passwordStrength/script.js
+++ src/passwordStrength/script.js
@@ -1,10 +1,12 @@
 let password = document.getElementById('password');
 let power = document.getElementById('power-point');
 
-password.oninput = function(){
+password.oninput = function () {
    let point = 0;
    let value = password.value;
-   if(value.length >= 6){
+   let widthPower = ['1%', '25%', '50%', '75%', '100%'];
+   let colorPower = ['#d73f40', '#dc6551', '#f2b84f', '#beb952', '#30cec7']
+   if (value.length >= 6) {
       let arrayTest = [
          /[0-9]/,
          /[a-z]/,
@@ -12,10 +14,11 @@ password.oninput = function(){
          /[^0-9a-zA-Z]/
       ];
       arrayTest.forEach(item => {
-         if(item.test(value)){
+         if (item.test(value)) {
             point = point + 1;
          }
       })
+
       // if(/[0-9]/.test(value)){
       //    power = point + 1;
       // }
@@ -28,5 +31,14 @@ password.oninput = function(){
       // if(/[^0-9a-zA-Z]/.test(value)){
       //    power = power + 1;
       // }
+
+      power.style.width = widthPower[point];
+      power.style.backgroundColor = colorPower[point];
    }
+   else {
+      power.style.width = widthPower[0];
+      power.style.backgroundColor = colorPower[0];
+   }
+
+   console.log(point);
 }
